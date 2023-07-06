c99b5813c36feb3bd34b43b0f97ae8f4f74985bc
vishwa-akshat <akvs0707@gmail.com>
2023-07-06 22:22:14 +0530

    Added card render condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import RatingCard from "./components/RatingCard";
 import ThankYouCard from "./components/ThankYouCard";
 
+import { useGlobalStore } from "./store/globalStore";
+
 import "./App.scss";
 
 function App() {
+    const isRatingSubmitted = useGlobalStore(
+        (state) => state.isRatingSubmitted
+    );
     return (
         <div className="container">
-            {/* <RatingCard /> */}
-            <ThankYouCard />
+            {isRatingSubmitted ? <ThankYouCard /> : <RatingCard />}
         </div>
     );
 }
